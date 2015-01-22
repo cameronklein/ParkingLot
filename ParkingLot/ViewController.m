233f571a94eb0d97bfbb75ecf3614aa6f006d917
Cameron Klein <cameron.klein@gmail.com>
2015-01-21 23:05:50 -0800

    removed excess code from view controller

--- ParkingLot/ViewController.m
+++ ParkingLot/ViewController.m
@@ -17,26 +17,6 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   
-  ParkingLot *lot = [[ParkingLot alloc] initWithCapacity:50];
-  [lot parkVehicle:[CompactCar new]];
-  [lot parkVehicle:[FullCar new]];
-  [lot parkVehicle:[FullCar new]];
-  [lot parkVehicle:[FullCar new]];
-  [lot parkVehicle:[ElectricCar new]];
-  [lot parkVehicle:[ElectricCar new]];
-  [lot parkVehicle:[ElectricCar new]];
-  [lot parkVehicle:[ElectricCar new]];
-  [lot parkVehicle:[[Truck alloc] initWithNumberOfWheels:8]];
-  [lot parkVehicle:[[Truck alloc] initWithNumberOfWheels:8]];
-  [lot parkVehicle:[[Truck alloc] initWithNumberOfWheels:8]];
-  [lot parkVehicle:[[Truck alloc] initWithNumberOfWheels:4]];
-  [lot parkVehicle:[[Truck alloc] initWithNumberOfWheels:8]];
-  NSLog(@"%@", [lot listAllVehicles]);
-  NSLog(@"%@", [lot listVehiclesSortedByID]);
-  NSLog(@"%@", [lot listVehiclesSortedByType]);
-  NSLog(@"MoneyCollected From Trucks: %ld", [lot moneyEarnedFromVehicleType:[Truck new]]);
-  
-  
 }
 
 - (void)didReceiveMemoryWarning {
